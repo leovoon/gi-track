84757bfd204d731679c041e96ee030d322de9d5b
Leo Voon <voonlihhaur@gmail.com>
2023-06-23 13:08:40 +0800

    Add missing sign in component

--- src/pages/PublicPage.tsx
+++ src/pages/PublicPage.tsx
@@ -1,5 +1,13 @@
+import { SignIn } from "@clerk/clerk-react";
+
 const PublicPage = () => {
-  return <></>;
+  return (
+    <>
+      <div className="grid place-items-center h-1/2 mt-12">
+        <SignIn />
+      </div>
+    </>
+  );
 };
 
 export default PublicPage;

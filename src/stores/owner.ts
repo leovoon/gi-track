13154d4373a919259486da73844e1cc3813b8260
Leo Voon <voonlihhaur@gmail.com>
2023-07-03 15:49:41 +0800

    Persist owner filter state

--- src/stores/owner.ts
+++ src/stores/owner.ts
@@ -1,11 +1,19 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface Owner {
   isOwner: boolean;
   setIsOwner: (isOwner: Owner["isOwner"]) => void;
 }
 
-export const useOwnerStore = create<Owner>((set) => ({
-  isOwner: false,
-  setIsOwner: (isOwner: Owner["isOwner"]) => set({ isOwner }),
-}));
+export const useOwnerStore = create<Owner>()(
+  persist(
+    (set) => ({
+      isOwner: false,
+      setIsOwner: (isOwner: Owner["isOwner"]) => set({ isOwner }),
+    }),
+    {
+      name: "owner-storage",
+    }
+  )
+);
